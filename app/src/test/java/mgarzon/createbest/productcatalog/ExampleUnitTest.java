ba2dfae92e3cef391bc9c875aa5ac126424b744f
Aaditya Shah <ashah203@uottawa.ca>
2022-11-15 15:38:55 -0500

    changes to expresso testing as well as adding tests

--- app/src/test/java/mgarzon/createbest/productcatalog/ExampleUnitTest.java
+++ app/src/test/java/mgarzon/createbest/productcatalog/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package mgarzon.createbest.productcatalog;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +13,24 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void checkProductName() {
+        Product aProduct = new Product("1", "DELL MONITOR", 180);
+        assertEquals("Check the name of the product", "DELL MONITOR", aProduct.getProductName());
+    }
+
+    @Test
+    public void checkProductId() {
+        Product aProduct = new Product("1", "DELL MONITOR", 180);
+        assertEquals("Check the id of the product", "1", aProduct.getId());
+    }
+
+    @Test
+    public void checkProductPrice() {
+        // I am making this test to fail
+        Product aProduct = new Product("1", "DELL MONITOR", 180);
+        assertEquals("Check the price of the product", "0", aProduct.getPrice());
     }
 }
